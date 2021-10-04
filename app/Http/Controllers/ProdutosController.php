0b4af389a51fd95336f6cfebb2c5c48e912a2566
Romario Pires <romario.pires@grupolife.med.br>
2021-10-03 23:51:32 -0300

    Finalizando Api

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Produto::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $produto = new Produto();
+        $produto->nome      = $request->nome;
+        $produto->descricao = $request->descricao;
+        $produto->preco     = $request->preco;
+        $produto->save();
+        return $produto;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Produto $produto)
+    {
+        return $produto;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        $produto->nome      = $request->nome;
+        $produto->descricao = $request->descricao;
+        $produto->preco     = $request->preco;
+        $produto->save();
+        return $produto;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+    }
+}
